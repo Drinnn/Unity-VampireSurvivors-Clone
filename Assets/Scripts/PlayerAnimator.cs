3e4b54d6989669d9a01e4b7b3d0b2104ff78649b
Drinnn <pedro_lontro@hotmail.com>
2023-05-27 11:58:00 -0300

    feat: add simple player movement animation

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private const string IS_MOVING = "IsMoving";
+
+    [SerializeField] private PlayerController _playerController;
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        _animator.SetBool(IS_MOVING, _playerController.IsMoving);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 2.8f;
 
+    public bool IsMoving => _isMoving;
+
+    private bool _isMoving;
+    
     private void Update()
     {
         HandleMovement();
@@ -15,5 +19,7 @@ public class PlayerController : MonoBehaviour
         Vector3 movementDirection = new Vector3(playerInput.x, playerInput.y, 0);
 
         transform.position += movementDirection * moveSpeed * Time.deltaTime;
+
+        _isMoving = movementDirection != Vector3.zero;
     }
 }
