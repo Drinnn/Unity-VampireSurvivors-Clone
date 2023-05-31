dbfe5e84e381b3f8242eb0ad6f8b5339a5313c77
Drinnn <pedro_lontro@hotmail.com>
2023-05-31 09:47:29 -0300

    feat: add spawning/despawning fireballs

--- Assets/Scripts/Weapons/SpinWeapon.cs
+++ Assets/Scripts/Weapons/SpinWeapon.cs
@@ -4,10 +4,34 @@ public class SpinWeapon : MonoBehaviour
 {
     [SerializeField] private Transform holder;
     [SerializeField] private float rotationSpeed = 200f;
+    
+    [SerializeField] private Transform weaponToSpawn;
+    [SerializeField] private float timeBetweenSpawns = 3f;
+    [SerializeField] private float weaponLifetime = 2f;
+
+    private float _spawnTimer;
 
     private void Update()
     {
-        holder.rotation =
-            Quaternion.Euler(0f, 0f, holder.rotation.eulerAngles.z + (rotationSpeed * Time.deltaTime));
+       HandleRotation();
+       HandleSpawns();
+    }
+
+    private void HandleRotation()
+    {
+        holder.rotation = Quaternion.Euler(0f, 0f, holder.rotation.eulerAngles.z + rotationSpeed * Time.deltaTime); 
+    }
+
+    private void HandleSpawns()
+    {
+        _spawnTimer -= Time.deltaTime;
+        if (_spawnTimer <= 0)
+        {
+            Transform weapon = Instantiate(weaponToSpawn, weaponToSpawn.position, weaponToSpawn.rotation, holder);
+            weapon.gameObject.SetActive(true);
+            Destroy(weapon.gameObject, weaponLifetime);
+            
+            _spawnTimer = timeBetweenSpawns;
+        }
     }
 }
