5248c8180c1ead3ae5169359005da7e694e557b7
Drinnn <pedro_lontro@hotmail.com>
2023-06-09 23:28:06 -0300

    feat: add levels specification

--- Assets/Scripts/ExpSystem/ExpSystem.cs
+++ Assets/Scripts/ExpSystem/ExpSystem.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ExpSystem : MonoBehaviour
@@ -5,12 +6,32 @@ public class ExpSystem : MonoBehaviour
     public static ExpSystem Instance { get; private set; }
 
     [SerializeField] private Transform expPickupPrefab;
+    [SerializeField] private ExpLevelConfigSO expLevelConfigSO;
 
+    private List<int> _expLevels; 
+        
     private float _currentXp;
+    private int _currentLevel;
     
     private void Awake()
     {
         Instance = this;
+
+        _expLevels = new List<int>();
+        for (int i = 0; i <= expLevelConfigSO.levelAmount; i++)
+        {
+            if (i == 0)
+            {
+                _expLevels.Add((int)expLevelConfigSO.initialLevelExp);
+            }
+            else
+            {
+                _expLevels.Add(Mathf.CeilToInt(_expLevels[i - 1] * expLevelConfigSO.levelMultiplier));
+            }
+        }
+
+        _currentXp = 0;
+        _currentLevel = 1;
     }
 
     public void AddXp(float amount)
---
+++ Assets/Scripts/ScriptableObjects/ExpLevelConfigSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ExpLevelConfigSO : ScriptableObject
+{
+    public int levelAmount;
+    public float initialLevelExp;
+    public float levelMultiplier;
+}
